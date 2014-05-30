e9950a08a7a1e3c48af45a80582c3a460108e5dc
Ariyasuk Kittikorn <ariyasuk-k@klab.com>
2014-05-30 20:20:30 +0900

    whatever

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,13 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return render_template('tor.html')
 
+if __name__ == '__main__':
+    app.run()
