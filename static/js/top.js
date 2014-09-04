79598b65a7c02c31b1fec0d016420787fd24366f
Ariyasuk Kittikorn <kittikorn.a@gmail.com>
2014-09-04 20:06:01 +0900

    update

--- static/js/top.js
+++ static/js/top.js
@@ -1,5 +1,6 @@
 // Hide Header on on scroll down
 var didScroll;
+var firstScroll = false;
 var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('header').outerHeight();
@@ -79,18 +80,28 @@ $(window).scroll(function(event){
 
 setInterval(function() {
     if (didScroll) {
+        if(!firstScroll){
+          firstScrolled();
+          firstScroll = true;
+        }
         hasScrolled();
         didScroll = false;
     }
 }, 250);
 
-
+function firstScrolled(){
+  $('.fa.fa-arrow-down.arrow').addClass('hidden');
+}
 
 function hasScrolled() {
     var st = $(this).scrollTop();
-
+    if(st > 50) {
+        $('.home-menu.pure-menu-open').removeAttr('id');
+    } else {
+        $('.home-menu.pure-menu-open').attr('id', 'transparent');
+    }
     // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
+    /*if(Math.abs(lastScrollTop - st) <= delta)
         return;
 
     // If they scrolled down and are past the navbar, add class .nav-up.
@@ -109,12 +120,15 @@ function hasScrolled() {
         }
     }
 
-    lastScrollTop = st;
+    lastScrollTop = st;*/
 }
 
 $(document).ready(function() {
   slide_menu();
-  $('#splash_animate').removeClass().addClass('splash animated bounceInRight').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-      $(this).removeClass().addClass('splash');
+  $('#splash_animate').removeClass().addClass('splash-head animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      $(this).removeClass().addClass('splash-head');
+      $('#splash_animate2').removeClass().addClass('splash-subhead animated fadeInDown').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+          $(this).removeClass().addClass('splash-subhead');
+        });
     });
 });
